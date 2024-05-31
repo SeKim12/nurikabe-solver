412fda78ee17140fa5df600c2185ca10febfb39f
Ji Hun Wang <46038281+JiHunWang@users.noreply.github.com>
2024-05-30 18:11:33 -0700

    Delete Nurikabe directory

--- Nurikabe/alpha_nurikabe/__init__.py
+++
@@ -1,3 +0,0 @@
-from .alpha_nurikabe import AlphaNurikabe
-
-__all__ = ["AlphaNurikabe"]
--- Nurikabe/utils/__init__.py
+++
@@ -1,9 +0,0 @@
-from .utils import PATH_TO_CSV, SIZE, UnsolvableError, FillTerminalGrid,\
-    SoftmaxMap
-from .data_transform import custom_encoder, read_transform
-from .grid import SmartGrid, Grid
-from .nurikabe_grid import NurikabeGrid
-
-__all__ = ["Grid", "SmartGrid", "PATH_TO_CSV", "SIZE", "UnsolvableError",
-           "FillTerminalGrid", "custom_encoder", "read_transform",
-           "NurikabeGrid", "SoftmaxMap"]
