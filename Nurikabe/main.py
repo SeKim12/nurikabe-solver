e8df8a52772f66ee20c5c309e4c34e643bb2927f
Ji Hun Wang <46038281+JiHunWang@users.noreply.github.com>
2024-05-30 18:16:19 -0700

    Remove commented code from main.py

--- Nurikabe/main.py
+++ Nurikabe/main.py
@@ -1,7 +1,4 @@
-# from utils import read_transform
-# from backtrack import BacktrackSolver
 from mcts import MCTS
-# from deep_iterative_solver import DeepIterativeSolver
 from alpha_nurikabe import AlphaNurikabe
 from tensorflow.keras.models import load_model
 import time
@@ -9,7 +6,6 @@ import numpy as np
 
 NROWS = 2
 DATASET_PATH = './assets/nurikabe.npz'
-# data_X, data_Y = read_transform(NROWS=NROWS)
 
 all_solutions = np.load(DATASET_PATH)
 
@@ -28,28 +24,12 @@ model = load_model('./alpha_nurikabe/policy_network.keras')
 
 for i in range(1, NROWS):
     print('---------', i)
-    # print('--------- Backtrack ')
-    # start_time = time.time()
-    # # back_solver = BacktrackSolver(data_X[i])
-    # back_solver = BacktrackSolver(single_board)
-    # print(back_solver.solve())
-    # print(round(time.time() - start_time, 2))
-    # print(back_solver.iterations)
-
     print('--------- MCTS ')
     start_time = time.time()
     mcts_solver = MCTS(single_board, single_solution, max_iterations=20000)
-    # mcts_solver = MCTS(data_X[i], max_iterations=100000)
     print(mcts_solver.solve())
     print(round(time.time() - start_time, 2))
     print(mcts_solver.iterations)
-
-    # print('--------- DeepIterativeSolver ')
-    # start_time = time.time()
-    # deep_solver = DeepIterativeSolver(data_X[i], model=model)
-    # deep_solver.solve()
-    # print(round(time.time() - start_time, 2))
-    # print(deep_solver.iterations)
 
     print('--------- AlphaNurikabe ')
     start_time = time.time()
